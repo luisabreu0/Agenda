f4289daf406e611eddb7a488dddaa9aa7b2a6be2
luisabreu0 <luis_1k@hotmail.com>
2015-07-30 23:52:49 -0300

    Comit Inicial

---
+++ Agenda01/src/main/java/br/com/hue/mb/UsuarioManagedBean.java
@@ -0,0 +1,12 @@
+package br.com.hue.mb;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class UsuarioManagedBean implements Serializable{
+
+	private static final long serialVersionUID = -6160176064022594760L;
+	//viagei
+}
